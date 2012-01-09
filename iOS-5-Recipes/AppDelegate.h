9e476f5e5ee02b8f5915bef1efcd61b8e631a854
YANG Yuxin <bob.yuxinyang@gmail.com>
2012-01-09 13:53:59 +0800

    Initial Commit

---
+++ iOS-5-Recipes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS-5-Recipes
+//
+//  Created by 杨裕欣 on 12-1-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS-5-Recipes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS-5-Recipes
+//
+//  Created by 杨裕欣 on 12-1-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
